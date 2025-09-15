52ac5242f063b358493aceefff1ec01fac3dcf8a
ShihabPalekar <shihabpalekar23@gmail.com>
2025-09-15 08:15:16 +0530

    installed ag-grid

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+
 createRoot(document.getElementById('root')!).render(
   <>
     Initial Setup
